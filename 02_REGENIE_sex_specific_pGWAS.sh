c257571960694fcaed60057a5dc8146ece6be727
koprulumine <90835584+koprulumine@users.noreply.github.com>
2025-03-20 11:07:15 +0000

    Update 02_REGENIE_sex_specific_pGWAS.sh

--- 02_REGENIE_sex_specific_pGWAS.sh
+++ 02_REGENIE_sex_specific_pGWAS.sh
@@ -10,14 +10,14 @@
 --phenoFile /insert_path/input/relevant_phenotype_file.txt \
 --covarColList age,age2,PC1,PC2,PC3,PC4,PC5,PC6,PC7,PC8,PC9,PC10 \
 --catCovarList chip,Batch \
---keep /insert_path/input/UKBB_Olink_women_only.txt \
+--keep /insert_path/input/UKBB_Olink_female_only.txt \
 --extract /insert_path/input/High_QC_filtered_UKBB_SNPs.prune.in \
 --qt \
 --bsize 1000 \
 --threads 8 \
 --lowmem \
 --lowmem-prefix tmpdir/regenie_tmp_preds \
---out step1_UKBB_Olink/fit_bin_out_UKBB_Olink_step1
+--out step1_UKBB_Olink_female/fit_bin_out_UKBB_Olink_step1_female
 
 for i in {1..23}; do ~/Programs/regenie/regenie_v3.1.4.gz_x86_64_Centos7_mkl \
 --step 2 \
@@ -27,13 +27,13 @@ for i in {1..23}; do ~/Programs/regenie/regenie_v3.1.4.gz_x86_64_Centos7_mkl \
 --covarFile /insert_path/input/relevant_covariates_file.txt \
 --covarColList age,age2,PC1,PC2,PC3,PC4,PC5,PC6,PC7,PC8,PC9,PC10 \
 --catCovarList chip,Batch \
---keep /insert_path/input/UKBB_Olink_women_only.txt \
+--keep /insert_path/input/UKBB_Olink_female_only.txt \
 --qt \
 --bsize 400 \
 --minMAC 50 \
 --minINFO 0.4 \
 --threads 30 \
 --gz \
---pred step1_UKBB_Olink/fit_bin_out_UKBB_Olink_step1_pred.list \
---out output/UKBB_trait_of_interest_women_only_chr"$NUMBER"; done
+--pred step1_UKBB_Olink_female/fit_bin_out_UKBB_Olink_step1_female_pred.list \
+--out output/UKBB_trait_of_interest_female_only_chr"$NUMBER"; done
 
