5e8dc1a000cf8e279981e8289142c9968e04fca3
koprulumine <90835584+koprulumine@users.noreply.github.com>
2025-03-20 11:05:54 +0000

    Update 01_fastGWA_sex_specific_pGWAS.sh

--- 01_fastGWA_sex_specific_pGWAS.sh
+++ 01_fastGWA_sex_specific_pGWAS.sh
@@ -6,10 +6,10 @@
 ~/Programs/GCTA/gcta_1.93.2beta/gcta64 \
 --mbgen /insert_path/sex-specific-pGWAS/input/bgen_final.txt \
 --sample /insert_path/sex-specific-pGWAS/input/Fenland_QCed_samples.txt \
---keep /insert_path/sex-specific-pGWAS/input/Fenland_women_only_samples_keep.txt \
+--keep /insert_path/sex-specific-pGWAS/input/Fenland_female_only_samples_keep.txt \
 --grm-sparse /insert_path/sex-specific-pGWAS/input/Fenland_GRM_bgen-sparse \
 --fastGWA-mlm \
 --pheno /insert_path/sex-specific-pGWAS/input/Fenland_SomaLogic_invn_proteomics.txt \
 --mpheno insert_the_order_of_trait_to_analyse_from_pheno_file \
 --geno 0.05 \
---out /insert_path/sex-specific-pGWAS/output/Fenland_trait_of_interest_women_only
+--out /insert_path/sex-specific-pGWAS/output/Fenland_trait_of_interest_female_only
